3d2aa967e8ee119975f02ce67693302ce1f8a718
vincearon99 <rojalesvincearon@gmail.com>
2026-06-05 14:47:43 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body class="bg-light">
+<body class="bg-color:f5f5dc">
 
 <div class="container d-flex justify-content-center align-items-center vh-100">
 
